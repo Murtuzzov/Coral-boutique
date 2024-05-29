3cb550368b542ba393187297b15a0d15acb5f665
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-29 23:00:49 +0400

    добавлена секция slider и brand

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { productsFunc } from "./slider-list-products";
+
+productsFunc(3)
+  .then((result) => {
+    return result;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
