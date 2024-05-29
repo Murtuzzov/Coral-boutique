b2bb967157995dbda0be77f1e687cddaf3d6f3f3
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-29 21:32:16 +0400

    изменение стили

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+import { productsFunc } from "./slider-list-products";
+productsFunc(3)
+  .then((result) => {
+    return result;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
