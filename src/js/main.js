477e933f1926c9dcd82cf00ad1236519eab32cd0
Murtuzzov <131525655+Murtuzzov@users.noreply.github.com>
2024-05-30 12:33:19 +0400

    удаление кода Кямрана

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { productsFunc } from "./slider-list-products";
-
-productsFunc(3)
-  .then((result) => {
-    return result;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
