e762642b1fb1c98f546404974ca8c5132984e886
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-31 20:49:45 +0400

    Добавлено иконки для секции слайдера

--- src/js/slider-list-products.js
+++ src/js/slider-list-products.js
@@ -11,18 +11,17 @@ export const productsFunc = async () => {
       sliderListItem.classList.add("slider-list__item");
       sliderListItem.innerHTML = `
         <div class="slider-list__item-inner">
-        <div class="slider-list__products-add-cart">
-        <img 
-          src="./images/slider-products/icons-heart.png"
-          alt="HEART" width="24" height="24"
-        />
-      </div>
-        <img class="slider-list__items-img" src="${product.photo}" alt="Products" width="312" height="400" />
-        <p class="slider-list__items-name">${product.name}</p>
-        <div class="slider-list__products-info">
-            <p class="slider-list__items-type">${product.type}</p>
-            <p class="slider-list__items-price">${product.price}</p>
-        </div>
+          <div class="slider-list__products-add-cart">
+             <img 
+                src="./images/slider-products/icons-heart.png"
+                alt="HEART" width="24" height="24"/>
+          </div>
+              <img class="slider-list__items-img" src="${product.photo}" alt="Products" width="312" height="400" />
+              <p class="slider-list__items-name">${product.name}</p>
+                <div class="slider-list__products-info">
+                  <p class="slider-list__items-type">${product.type}</p>
+                  <p class="slider-list__items-price">${product.price}</p>
+            </div>
         </div>
       `;
       sliderListProducts.appendChild(sliderListItem);
