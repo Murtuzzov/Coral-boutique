c2036b0afca56d0c020df9d9e5b6195ad833aa68
Gorkhmaaz <gosha.alekbarov@mail.ru>
2024-06-04 23:37:50 +0300

    add js for basket

--- src/js/main.js
+++ src/js/main.js
@@ -24,17 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const showRegisterLink = document.getElementById("showRegister");
   const showLoginLink = document.getElementById("showLogin");
 
-  // Sayfa yüklendiğinde varsayılan görünürlük ayarları
-  if (localStorage.getItem('registered') === 'true') {
-      loginForm.style.display = 'block';
-      registerForm.style.display = 'none';
-      showRegisterLink.style.display = 'none';
-      showLoginLink.style.display = 'block';
-  } else {
-      loginForm.style.display = 'none';
-      registerForm.style.display = 'block';
-      showRegisterLink.style.display = 'block';
-      showLoginLink.style.display = 'none';
+  if (localStorage.getItem("registered") === "true") {
+    loginForm.style.display = "block";
+    registerForm.style.display = "none";
+    showRegisterLink.style.display = "none";
+    showLoginLink.style.display = "none";
   }
 
   accountButton.addEventListener("click", function (event) {
@@ -122,61 +116,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  showRegisterLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      loginForm.style.display = 'none';
-      registerForm.style.display = 'block';
-      showRegisterLink.style.display = 'none';
-      showLoginLink.style.display = 'block';
-  });
-
-  showLoginLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      registerForm.style.display = 'none';
-      loginForm.style.display = 'block';
-      showRegisterLink.style.display = 'block';
-      showLoginLink.style.display = 'none';
-  });
-
-  registerForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      let email = document.getElementById('registerEmail').value;
-      let password = document.getElementById('registerPassword').value;
-      let confirmPassword = document.getElementById('registerConfirmPassword').value;
-
-      if (password !== confirmPassword) {
-          alert('Passwords do not match');
-          return;
-      }
-
-      localStorage.setItem('registered', 'true');
-      localStorage.setItem('email', email);
-      alert('Registration successful');
-      registerForm.reset();
-      registerForm.style.display = 'none';
-      loginForm.style.display = 'block';
-      showRegisterLink.style.display = 'none';
-      showLoginLink.style.display = 'none';
-  });
-
-  loginForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      let email = document.getElementById('loginEmail').value;
-      let password = document.getElementById('loginPassword').value;
-
-      alert('Login successful');
-      loginForm.reset();
-      const myModal = bootstrap.Modal.getInstance(document.getElementById('loginModal'));
-      myModal.hide();
-  });
-});
-
-
-
-
-
-
-
+  document
+    .querySelector(".modal__product__section__cart")
+    .addEventListener("mouseover", function () {
+      this.style.cursor = "pointer";
+    });
 
   document
     .querySelector(".modal__product__section__cart")
