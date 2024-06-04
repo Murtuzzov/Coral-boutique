5129a6a2c8579739c8871c3f9cfd32ffe249d54f
Gorkhmaaz <gosha.alekbarov@mail.ru>
2024-06-04 20:25:31 +0300

    add to cart in shopping

---
+++ src/js/data.js
@@ -0,0 +1,10 @@
+export const products = [
+    { id: 1, imgSrc: "images/sponsors/joggersImg.png", categoryName: "Dress", title: "Adicolor Classics Joggers", price: "63.85" },
+    { id: 2, imgSrc: "images/sponsors/nikeAirImg.png", categoryName: "Bag", title: "Nike Sportswear Futura Luxe", price: "130.00" },
+    { id: 3, imgSrc: "images/sponsors/scarfImg.png", categoryName: "Scarf", title: "Geometric Print Scarf", price: "53.00" },
+    { id: 4, imgSrc: "images/sponsors/hoodieImg.png", categoryName: "Dress", title: "Yellow Reserved Hoodie", salePrice: "364.00", price: "130.00" },
+    { id: 5, imgSrc: "images/sponsors/dressGreenImg.png", categoryName: "Dress", title: "Basic Dress Green", price: "236.00" },
+    { id: 6, imgSrc: "images/sponsors/nikeAirImg.png", categoryName: "Shoe", title: "Nike Air Zoom Pegasus", salePrice: "220.00", price: "198.00" },
+    { id: 7, imgSrc: "images/sponsors/nikeMiler.png", categoryName: "Dress", title: "Nike Repel Miler", price: "120.50" },
+    { id: 8, imgSrc: "images/sponsors/nikeGlassesImg.png", categoryName: "Glasses", title: "Nike Sportswear Futura Luxe", price: "160.00" },
+  ];
